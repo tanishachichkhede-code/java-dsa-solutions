d1346bb2f5b1ed6d1bac53775dd5a2e30a5c4eea
Tanisha Chichkhede <tanishachichkhede@gmail.com>
2026-07-27 12:51:45 +0530

    Initial commit with Two Sum solution

---
+++ Arrays/TwoSum.java
@@ -0,0 +1,28 @@
+/*
+Problem: Two Sum
+Platform: LeetCode
+Difficulty: Easy
+
+Approach:
+- Brute Force
+- Check every pair
+
+Time Complexity: O(n²)
+Space Complexity: O(1)
+*/
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+    for(int i=0;i<nums.length;i++){
+        for(int j=i+1;j<nums.length;j++){
+            if(nums[i]+nums[j]==target){
+               return new int[] {i,j};
+            }
+             
+        }
+
+    }
+        return new int[] {};
+     
+     
+         }
+}
