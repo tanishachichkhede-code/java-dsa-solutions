c608cb2b3755bd9244d03932599100a795b75c4c
Tanisha Chichkhede <tanishachichkhede@gmail.com>
2026-07-31 23:57:07 +0530

    Added linked list and palindrome solutions

---
+++ LinkedList/LinkedListMiddle.java
@@ -0,0 +1,33 @@
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        int count=0;
+        ListNode current=head;
+        while(current!=null){
+            count++;
+            current =current.next;
+            
+        }
+        int middle =count/2;
+        current = head;
+        for (int i = 0; i < middle; i++) {
+            current = current.next;
+        }
+        return current;
+    }
+}
